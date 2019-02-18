95feb93fc4dc547632542d2269e481cc4255d799
Jaewoo Maeng <maeng9584@gmail.com>
2019-02-18 16:20:39 +0900

    testing if form is valuable

--- default-theme.js
+++ default-theme.js
@@ -2585,7 +2585,6 @@ var EmailSignInForm = function (_React$Component) {
       alert("theme");
       var formData = this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "form"]).toJS();
       formData.number = 111;
-      formData.cookie = props.cookie;
       this.props.dispatch((0, _emailSignIn.emailSignIn)(formData, this.getEndpoint())).then(this.props.next).catch(function () {});
     }
   }, {
