4baaeffc7e690331ea0889d9ed4a07844f26893f
Jaewoo Maeng <maeng9584@gmail.com>
2019-02-20 17:10:56 +0900

    function work?

--- index.js
+++ index.js
@@ -1125,14 +1125,10 @@ function emailSignIn(body, endpointKey) {
       },
       method: "post",
       body: JSON.stringify(body)
-<<<<<<< HEAD
     }).then(function (parseResponse) {
       alert(JSON.stringify(parseResponse));
       return parseResponse;
     }).then(function (user) {
-=======
-    }).then(alert(response)).then(_handleFetchResponse.parseResponse).then(function (user) {
->>>>>>> 67709e777bb390971a6be329fd8f24d65001280b
       return dispatch(emailSignInComplete(currentEndpointKey, user));
     }).catch(function (errors) {
       // revert endpoint key to what it was before failed request
