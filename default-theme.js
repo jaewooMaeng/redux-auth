9e1f98ada6d47ecc25b05ada5eaa61b3ff5f9c77
Jaewoo Maeng <maeng9584@gmail.com>
2019-04-09 23:45:08 +0900

    cookie -> number

--- default-theme.js
+++ default-theme.js
@@ -2620,7 +2620,7 @@ var EmailSignInForm = function (_React$Component) {
       console.log(history);
       event.preventDefault();
       var formData = this.props.auth.getIn(["emailSignIn", this.getEndpoint(), "form"]).toJS();
-      formData.number = this.props.inputProps.cookie;
+      formData.number = this.props.inputProps.number;
       this.props.dispatch((0, _emailSignIn.emailSignIn)(formData, this.getEndpoint())).then(this.props.next).catch(function () {});
     }
   }, {
